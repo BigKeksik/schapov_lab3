6db490e334d0072b64927521a80f87f022f84805
Evgeniy Malyakshin <bigkeksik@bk.ru>
2024-03-05 21:50:07 +0500

    add default function

--- lab3.cpp
+++ lab3.cpp
@@ -1,7 +1,43 @@
 #include <iostream>
 
- using namespace std;
+using namespace std;
 
- int main() {
-     cout << "Hello, world!" << endl;
- }
+double** createMatrix(int length, bool isSecond) {
+    double** matrix = new double* [length];
+    for (int i = 0; i < length; i++) {
+        if (!isSecond) {
+            matrix[i] = new double[length];
+            for (int j = 0; j < length; j++) {
+                matrix[i][j] = rand() % 10;
+            }
+        } else {
+            matrix[i] = new double[1];
+            matrix[i][0] = rand() % 10;
+        }
+    }
+        
+    return matrix;
+}
+
+void printMatrix(double** matrix, int length, bool isSecond) {
+    for (int i = 0; i < length; i++) {
+        if (!isSecond) {
+            for(int j = 0; j < length; j++) {
+                cout << matrix[i][j] << ' ';
+            }
+        } else {
+            cout << matrix[i][0];
+        }
+        
+        cout << endl;
+    }
+}
+
+int main() {
+    int length = 3;
+    double** matrix = createMatrix(length, false);
+    
+    printMatrix(matrix, length, false);
+    
+    return 0;
+}
